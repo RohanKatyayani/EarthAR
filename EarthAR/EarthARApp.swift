6427e4f21780a3e19d7355d9a1ee7e21d5e24d82
Rohan Katyayani <60040641+RohanKatyayani@users.noreply.github.com>
2026-04-11 01:28:53 +0530

    Initial Commit

---
+++ EarthAR/EarthARApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthARApp.swift
+//  EarthAR
+//
+//  Created by Rohan Katyayani on 11/04/26.
+//
+
+import SwiftUI
+
+@main
+struct EarthARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
